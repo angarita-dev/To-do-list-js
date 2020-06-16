b74912aa733c7947100f348e4ab47d0cff081cad
angarita <escobar.angarita.juan@gmail.com>
2020-06-15 20:11:26 -0500

    added Js factories & storage logic

---
+++ src/js/factories.js
@@ -0,0 +1,75 @@
+// Project logic
+function project({title='To-do',priority='low-priority'}){
+  let template = document.createElement('template');
+  let html = `
+    <li class='to-do-project'>
+      <div class='project-container text-container ${priority}'>
+        <h4 class='text'>${title}</h4>
+      </div>
+      <div class='edit-container'>
+        <input class='text edit-project'/>
+        <span class='low-priority priority-selector'></span>
+        <span class='medium-priority priority-selector'></span>
+        <span class='high-priority priority-selector'></span>
+        <span class='uh-priority priority-selector'></span>
+      </div>
+      <svg class='icon'>
+        <use class='edit-icon' xlink:href='#edit-icon' />
+        <use class='delete-icon delete-project-icon' xlink:href='#delete-icon' />
+      </svg>
+    </li>
+  `.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+// To-do logic
+
+function toDoList(toDoItemsList){
+  let toDoContainer = document.createElement('ul');
+  toDoContainer.classList.add('to-do-container');
+  toDoContainer.setAttribute('id','to-do-container');
+  toDoItemsList.map( toDoElement => {
+    let text;
+    let priority;
+    let checked;
+    ({text,priority,checked} = toDoElement);
+    toDoContainer.appendChild(
+      toDoItem({text,priority,checked})
+    );
+  });
+  return toDoContainer;
+}
+
+function toDoItem({text,priority,checked=false}){
+  console.log({text,priority,checked});
+  let template = document.createElement('template');
+  let checkedClass = checked ? 'checked' : '';
+  let html = `
+    <li class="to-do-item ${checkedClass}">
+      <p>${text}</p>
+      <span class="${priority} enter-edit-mode"/>
+    </li>
+  `.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+function toDoListEdit(value='') {
+  let template = document.createElement('template');
+  template.innerHTML = `
+    <li class="to-do-item-edit" id='to-do-item-edit'>
+      <input class="text edit-project" id='to-do-item-edit-input' value="${value}" placeholder="To do Item name"/>
+      <span class="low-priority to-do-priority-selector"></span>
+      <span class="medium-priority to-do-priority-selector"></span>
+      <span class="high-priority to-do-priority-selector"></span>
+      <span class="uh-priority to-do-priority-selector"></span>
+       <svg class='icon'>
+        <use class='delete-icon' xlink:href='#delete-icon' />
+      </svg>
+    </li>
+  `.trim();
+  return template.content.firstChild;
+}
+
+export { project, toDoList, toDoItem, toDoListEdit };
---
+++ src/js/storage.js
@@ -0,0 +1,56 @@
+// Project logic
+function saveProject({title, priority='low-priority',index=-1}){
+  let newProjects;
+  let oldProjects = localStorage.getItem('projects');
+
+  if (oldProjects == null){
+    newProjects = [{title, priority, toDo:[]}]
+  } else if(index == -1) {
+    // Should append project
+    newProjects = [JSON.parse(oldProjects),{title,priority,toDo:[]}].flat();
+  } else {
+    newProjects = JSON.parse(oldProjects);
+    newProjects[index].title = title;
+    newProjects[index].priority = priority;
+  }
+
+  localStorage.clear();
+  localStorage.setItem('projects', JSON.stringify(newProjects));
+}
+
+function readProject(index) {
+  let projects = JSON.parse(localStorage.getItem('projects'));
+
+  return projects[index];
+}
+
+function deleteProject(index){
+  let projects = JSON.parse(localStorage.getItem('projects'));
+  projects.splice(index,1);
+  
+  localStorage.clear();
+  localStorage.setItem('projects', JSON.stringify(projects));
+}
+
+function readProjects() {
+  let projects = JSON.parse(localStorage.getItem('projects'));
+  if(projects == null) return [];
+
+  return projects;
+}
+
+// To-Do logic
+function saveToDo({index, toDoList}){
+  if(index == -1) return;
+  let newProjects;
+  let oldProjects = localStorage.getItem('projects');
+
+  newProjects = JSON.parse(oldProjects);
+  console.log( newProjects[index].toDo);
+  newProjects[index].toDo = toDoList;
+
+  localStorage.clear();
+  localStorage.setItem('projects', JSON.stringify(newProjects)); 
+}
+
+export { saveProject, readProject, readProjects, saveToDo, deleteProject };
