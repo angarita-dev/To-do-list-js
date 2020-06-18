3a6a5ac7ae6257ace83d591c16829622e614df03
angarita <44899916+codingAngarita@users.noreply.github.com>
2020-06-17 21:08:07 -0500

    Fixed linting issues

--- src/js/storage.js
+++ src/js/storage.js
@@ -24,7 +24,6 @@ function readProject(index) {
 }
 
 function deleteProject(index) {
-  console.log(index)
   const projects = JSON.parse(localStorage.getItem('projects'));
   projects.splice(index, 1);
 
@@ -56,7 +55,6 @@ function shouldAddTutorial() {
 }
 
 function addTutorial() {
-  console.log('adding tutorial');
   const tutorialInfo = [{ title: 'Tutorial', priority: 'uh-priority', toDo: [{ priority: 'uh-priority', checked: false, text: 'Welcome to the to-do-list-project' }, { priority: 'uh-priority', checked: false, text: 'You can order items by priority' }, { priority: 'uh-priority', checked: false, text: 'Click on the text to mark it as done' }, { priority: 'high-priority', checked: false, text: 'You can edit by clicking the priority dot' }, { priority: 'high-priority', checked: false, text: 'You can add items by clicking the plus button' }, { priority: 'medium-priority', checked: false, text: 'Or adding project on the left sidebar' }, { priority: 'low-priority', checked: false, text: 'Items will be sorted automatically' }, { priority: 'low-priority', checked: true, text: 'This item is checked' }] }];
 
   localStorage.setItem('tutorial', 'true');
