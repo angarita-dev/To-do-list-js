70b463a0f87517effd5188fe6c39b2d1b5829768
angarita.Dev <44899916+codingAngarita@users.noreply.github.com>
2020-07-08 17:22:02 -0500

    Added Display

---
+++ src/js/project/display.js
@@ -0,0 +1,80 @@
+import * as Factory from 'factory';
+class Display {
+	static removeSelected() {
+		const projectsSelected = document.getElementsByClassName('selected');
+
+		projectsSelected.forEach(project => project.classList.remove('selected')); 
+	}
+
+	selectProject() {
+		removeSelected();
+
+		this.projectContainer.classList.add('selected');
+	}
+
+	editProject(handleFunction) {
+		const editContainer = this.projectContainer.querySelector('.edit-container');
+		const prioritySelectors = this.projectContainer.querySelector('.priority-selector');	
+
+		prioritySelectors.forEach( prioritySelector => {
+			const projectName = editContainer.querySelector('#edit-project').value; 
+			const selectedPriority = Array.from(prioritySelector.classList)
+															   .filter(className => className.includes('-priority'))[0];
+			
+			this.projectContainer.classList.remove('to-do-edit');
+			handleFunction(projectName, selectedPriority);
+		});
+	}
+
+	static displayProject(title, priority) {
+		const projectsContainer = document.getElementById('projects-container');
+		const project = Factory.project(title, priority);
+
+		projectsContainer.append(project);
+		return project;
+	}
+
+	loaders(handleProjectEdit, handleProjectDelete) {
+		const iconContainer = this.projectContainer.querySelector('.icon');
+
+		const iconEdit = iconContainer.querySelector('.edit-icon');
+		const iconDelete = iconContainer.querySelector('.delete-icon');
+
+		this.projectContainer.onclick = () => { selectProject() };
+		iconEdit.onclick = () => { 
+			this.projectContainer.classList.add('to-do-edit');
+			handleProjectEdit();
+		};
+		iconDelete.onclick = () => { handleProjectDelete() };
+	}
+
+	constructor(title, priority, handleProjectEdit, handleProjectDelete) {
+		this.projectContainer = displayProject(title, priority);
+
+		loaders(handleProjectEdit, handleProjectDelete);
+	}
+
+	changeTitle(newTitle) {
+		const textContainerElement = this.projectContainer.querySelector('.text-container');	
+		const titleElement = titleElement.querySelector('.text');
+
+		titleElement.textContent = newTitle;
+	}
+
+	changePriority(newPriority) {
+		const priorityContainer = this.projectContainer.firstElementChild;
+
+		const previousPriority = priorityContainer.classList.filter( className => { 
+			return className.includes('-priority')
+		})[0]; 
+
+		priorityContainer.classList.remove(previousPriority);
+		priorityContainer.classList.add(newPriority);
+	}
+
+	removeElement() {
+		this.projectContainer.remove();
+	}
+}
+
+export default Display;
