2eb173632283d374dca988fa93df38fb798cf05d
angarita.Dev <44899916+codingAngarita@users.noreply.github.com>
2020-07-14 17:35:21 -0500

    Changed project edit

--- src/js/project/factory.js
+++ src/js/project/factory.js
@@ -8,17 +8,34 @@ class Factory {
         </div>
         <div class='edit-container'>
           <input class='text edit-project'/>
-          <span class='low-priority priority-selector'></span>
-          <span class='medium-priority priority-selector'></span>
-          <span class='high-priority priority-selector'></span>
-          <span class='uh-priority priority-selector'></span>
+          <div class="priority-edit">
+            <svg class="icon priority-icon low-priority">
+              <use xlink:href="#empty-priority"/>
+              <use class="full-priority" xlink:href="#full-priority"/>
+            </svg>
+            <svg class="icon priority-icon medium-priority">
+              <use xlink:href="#empty-priority"/>
+              <use class="full-priority" xlink:href="#full-priority"/>
+            </svg>
+            <svg class="icon priority-icon high-priority">
+              <use xlink:href="#empty-priority"/>
+              <use class="full-priority" xlink:href="#full-priority"/>
+            </svg>
+            <svg class="icon priority-icon uh-priority">
+              <use xlink:href="#empty-priority"/>
+              <use class="full-priority" xlink:href="#full-priority"/>
+            </svg>
+          </div>
         </div>
         <div class='icon-container'>
-          <svg class='icon'>
-              <use class='edit-icon' xlink:href='#edit-icon' />
+          <svg class='icon save-icon'>
+            <use class='save-icon' xlink:href='#save-icon' />
           </svg>
-          <svg class='icon'>
-              <use class='delete-icon delete-project-icon' xlink:href='#delete-icon' />
+          <svg class='icon edit-icon'>
+            <use class='edit-icon' xlink:href='#edit-icon' />
+          </svg>
+          <svg class='icon delete-icon'>
+            <use class='delete-icon delete-project-icon' xlink:href='#delete-icon' />
           </svg>
         </div>
       </li>
